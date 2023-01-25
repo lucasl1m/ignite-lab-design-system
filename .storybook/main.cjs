d84d08fd245362eba5d649fcfecd1f6e4e00cf5e
Lucas Araújo de Lima <70279700+lucasl1m@users.noreply.github.com>
2023-01-25 19:01:43 -0300

    feat: add storybook deployer dependencie

--- .storybook/main.cjs
+++ .storybook/main.cjs
@@ -1,18 +1,28 @@
+const { config } = require("@storybook/addon-actions")
+
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
-    "@storybook/addon-links",
-    "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
-  ],
-  "framework": "@storybook/react",
-  "core": {
-    "builder": "@storybook/builder-vite"
-  },
-  "features": {
-    "storyStoreV7": true
-  },
+    "stories": [
+        "../src/**/*.stories.mdx",
+        "../src/**/*.stories.@(js|jsx|ts|tsx)"
+    ],
+    "addons": [
+        "@storybook/addon-links",
+        "@storybook/addon-essentials",
+        "@storybook/addon-interactions"
+    ],
+    "framework": "@storybook/react",
+    "core": {
+        "builder": "@storybook/builder-vite"
+    },
+    "features": {
+        "storyStoreV7": true
+    },
+    viteFinal(config, { configType }) {
+        if (configType === 'DEVELOPMENT') {
+            config.base = '/ignite-lab-design-system/'
+
+            // do mutation to the config
+            return config
+        }
+    }
 }
